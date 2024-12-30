5ab4a81029476d9d72b0fc65fd855c3165f7d190
Lawrence D. Kern <law@lawkern.com>
2024-12-29 22:46:10 -0500

    Implement addressing modes for LDA instruction

--- src/cpu.adb
+++ src/cpu.adb
@@ -53,30 +53,95 @@ package body CPU is
          Put_Line ("LDA");
          Accumulator := Value;
 
-         Zero_Flag := (Value = 0);
+         Zero_Flag     := (Value = 0);
          Negative_Flag := (Shift_Right (Value, 7) and 1) = 1;
       end LDA;
 
-      Instruction, Family, Mode,  Modifier : U8;
+      function Merge (Low, High : U8) return U16 is
+      begin
+         return Shift_Left (U16 (High), 8) + U16 (Low);
+      end Merge;
 
-      Cycle_Count : Integer := 0;
+      Instruction, Family, Mode, Modifier : U8;
+
+      Cycle_Count        : Integer := 0;
       Instruction_Length : Integer := 1;
-   begin
-      Instruction := Memory (Program_Counter);
 
+      Data1, Data2 : U8 := 0;
+   begin
       Put_Line ("---------------------------------------------------");
 
+      Instruction := Memory (Program_Counter);
+
       case Instruction is
-         when 16#00# =>
+         when 16#00# => -- BRK
             Instruction_Length := 2; -- NOTE: BRK skips the following byte on return.
             Cycle_Count        := 7;
-
             BRK;
-         when 16#A9# =>
+
+         when 16#A9# => -- LDA #Immediate
             Instruction_Length := 2;
             Cycle_Count        := 2;
 
-            LDA (Memory (Program_Counter + 1));
+            Data1 := Memory (Program_Counter + 1);
+            LDA (Data1);
+
+         when 16#A5# => -- LDA Zero Page
+            Instruction_Length := 2;
+            Cycle_Count        := 3;
+
+            Data1 := Memory (Program_Counter + 1);
+            LDA (Memory (U16 (Data1)));
+
+         when 16#B5# => -- LDA Zero Page,X
+            Instruction_Length := 2;
+            Cycle_Count        := 4;
+
+            Data1 := Memory (Program_Counter + 1);
+            LDA (Memory (U16 (Data1 + Index_Register_X)));
+
+         when 16#AD# => -- LDA Absolute
+            Instruction_Length := 3;
+            Cycle_Count        := 4;
+
+            Data1 := Memory (Program_Counter + 1);
+            Data2 := Memory (Program_Counter + 2);
+            LDA (Memory (Merge (Low => Data1, High => Data2)));
+
+         when 16#BD# => -- LDA Absolute,X
+            Instruction_Length := 3;
+            Cycle_Count        := 4; -- TODO: 5 if page is crossed.
+
+            Data1 := Memory (Program_Counter + 1);
+            Data2 := Memory (Program_Counter + 2);
+            LDA (Memory (Merge (Low => Data1, High => Data2) + U16 (Index_Register_X)));
+
+         when 16#B9# => -- LDA Absolute,Y
+            Instruction_Length := 3;
+            Cycle_Count        := 4; -- TODO: 5 if page is crossed.
+
+            Data1 := Memory (Program_Counter + 1);
+            Data2 := Memory (Program_Counter + 2);
+            LDA (Memory (Merge (Low => Data1, High => Data2) + U16 (Index_Register_Y)));
+
+         when 16#A1# => -- LDA (Indirect,X)
+            Instruction_Length := 2;
+            Cycle_Count        := 6;
+
+            Data1 := Memory (Program_Counter + 1);
+            declare
+               Base : U16 := U16 (Data1) + U16 (Index_Register_X);
+            begin
+               LDA (Memory (Merge (Low => Memory (Base + 0), High => Memory (Base + 1))));
+            end;
+
+         when 16#B1# => -- LDA (Indirect),Y
+            Instruction_Length := 2;
+            Cycle_Count        := 5; -- TODO: 6 if page is crossed.
+
+            Data1 := Memory (Program_Counter + 1);
+            LDA (Memory (U16 (Memory (U16 (Data1))) + U16 (Index_Register_Y)));
+
          when others =>
             Put_Line ("*Unhandled Instruction*");
       end case;
